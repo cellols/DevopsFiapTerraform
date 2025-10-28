7977d412e26ca67ec55098e25c13251634bb92f7
cellols <150364889+cellols@users.noreply.github.com>
2025-10-27 21:34:53 -0300

    Remove cloud configuration from main.tf
    Removed cloud configuration for organization and workspace.

--- main.tf
+++ main.tf
@@ -15,14 +15,7 @@ terraform {
   }
   required_version = ">= 1.1.0"
 
-  cloud {
-    organization = "DevopsFiap"
-
-    workspaces {
-      name = "gh-actions"
-    }
   }
-}
 
 provider "aws" {
   region = "us-east-1"
